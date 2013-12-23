4085a9e0c10a92f1e5aa807af68a978c453ddd4a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-23 17:33:35 +0000

    [comments_on_albums] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26134 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on albums'] = 'Comentarios sobre los albumes';
 $lang['Display last posted comments on albums'] = 'Mostrar los últimos comentarios hechos en álbums.';
 $lang['Last comments on albums'] = 'Últimos comentarios en álbums.';
 $lang['Display comments on'] = 'Mostrar comentarios en';
+$lang['%d comments on albums'] = '%d comentarios en albums.';
+$lang['%d comment on album'] = '%d comentarios en el álbum';
+$lang['Photos'] = 'Fotos';
 ?>
