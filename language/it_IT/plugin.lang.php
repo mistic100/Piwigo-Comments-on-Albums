5c95d991cede9f27c746e577fb9df4601056c687
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-23 19:53:29 +0000

    [comments_on_albums] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26138 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on albums'] = 'Commenti sugli album';
 $lang['Display last posted comments on albums'] = 'Mostra ultimi commenti sugli album';
 $lang['Last comments on albums'] = 'Ultimi commenti sugli album';
 $lang['Display comments on'] = 'Mostra commenti su';
+$lang['%d comment on album'] = '%d commento su album';
+$lang['%d comments on albums'] = '%d commenti su album';
+$lang['Photos'] = 'Foto';
 ?>
