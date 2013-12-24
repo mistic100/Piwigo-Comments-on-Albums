7e43733c31c22584934332162ac30c89d7e81b6c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-24 08:33:05 +0000

    [comments_on_albums] Update nl_NL, thanks to : eijdenberg
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26165 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on photos'] = 'Commentaar op afbeeldingen';
 $lang['Display last posted comments on albums'] = 'Geef de laatste commentaren op albums weer';
 $lang['Last comments on albums'] = 'De laatste commentaren op albums';
 $lang['Display comments on'] = 'Geef reacties weer op';
+$lang['%d comment on album'] = '%d commentaar op album';
+$lang['%d comments on albums'] = '%d commentaar op albums';
+$lang['Photos'] = 'Fotos';
 ?>
