6e3693cdc5f1e4b6b22589ae09521b71fc6864fc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-28 10:16:08 +0000

    [comments_on_albums] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26280 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/plugin.lang.php
+++ language/el_GR/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on albums'] = 'Σχόλια σε άλμπουμς';
 $lang['Display last posted comments on albums'] = 'Εμφάνιση των τελευταίων δημοσιευμένων σχόλιων στα λευκώματα';
 $lang['Last comments on albums'] = 'Τελευταία σχόλια σε λευκώματα';
 $lang['Display comments on'] = 'Εμφάνιση σχολίων σχετικά με';
+$lang['Photos'] = 'Φωτογραφίες';
+$lang['%d comments on albums'] = '%d σχόλια στα λευκώματα';
+$lang['%d comment on album'] = '%d σχόλια στο λεύκωμα';
 ?>
