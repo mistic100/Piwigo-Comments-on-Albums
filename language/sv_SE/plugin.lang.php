1c6cae3dcdacd89c7211aa67ac715490f0b881da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-28 11:54:42 +0000

    [comments_on_albums] Update sv_SE, thanks to : magnus
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26287 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sv_SE/plugin.lang.php
+++ language/sv_SE/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on albums'] = 'Kommentarer till album';
 $lang['Display comments on'] = 'Visa kommentarer om';
 $lang['Display last posted comments on albums'] = 'Visa senaste kommentarer på album';
 $lang['Last comments on albums'] = 'Senaste kommentarer på album';
+$lang['Photos'] = 'Foton';
+$lang['%d comments on albums'] = '%d kommentarer till album';
+$lang['%d comment on album'] = '%d kommentarer till album';
 ?>
