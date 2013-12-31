52ccc8f42d9a00cd1f14c187d51394addf492a51
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-31 13:51:53 +0000

    [comments_on_albums] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26374 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on albums'] = 'Albüm Yorumları';
 $lang['Last comments on albums'] = 'Albümlerdeki Son yorumlar';
 $lang['Display last posted comments on albums'] = 'Albümlerdeki son yorumları görüntüle';
 $lang['Display comments on'] = 'Yorumu görüntüle';
+$lang['%d comment on album'] = 'albümde %d yorum';
+$lang['%d comments on albums'] = 'albümde %d yorum';
+$lang['Photos'] = 'Fotoğraflar';
 ?>
