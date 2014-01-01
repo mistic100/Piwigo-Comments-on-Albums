dd8bd22bd244987815d066dc5ff6549d04b77fd7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-01 19:54:29 +0000

    [comments_on_albums] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/Comments_on_Albums/trunk@26389 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/uk_UA/plugin.lang.php
+++ language/uk_UA/plugin.lang.php
@@ -5,4 +5,7 @@ $lang['Comments on photos'] = 'Коментарі до зображення';
 $lang['Display last posted comments on albums'] = 'Показати останні коментарі опубліковані до альбомів';
 $lang['Last comments on albums'] = 'Останні коментарі до альбомів';
 $lang['Display comments on'] = 'Показати коментарі';
+$lang['%d comment on album'] = '%d коментарій в альбомі';
+$lang['%d comments on albums'] = '%d коментарів в альбомах';
+$lang['Photos'] = 'Фотографії';
 ?>
