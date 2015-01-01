db57257e5f04f507d62cfb671e3c902ea07e0ba6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-01 20:00:02 +0100

    Update cs_CZ, thanks to jezekus

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -5,4 +5,6 @@ $lang['Comments on albums'] = 'Komentáře alb';
 $lang['Display last posted comments on albums'] = 'Zobraz poslední vytvořené komentáře alba';
 $lang['Last comments on albums'] = 'Poslední komentáře alba';
 $lang['Display comments on'] = 'Zobrazit komentáře';
-?>
+$lang['Photos'] = 'Fotografie';
+$lang['%d comments on albums'] = '%d komentáře alb';
+$lang['%d comment on album'] = '%d komentář alba';
