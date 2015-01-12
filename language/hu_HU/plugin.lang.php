1441e395f8a95524b17e4956563425e55c7a0820
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-12 06:16:43 +0100

    Update hu_HU, thanks to asicu.com

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -5,4 +5,6 @@ $lang['Comments on albums'] = 'Hozzászólások az album oldalakon';
 $lang['Display last posted comments on albums'] = 'Albumok utolsó hozzászólásainak megjelenítése';
 $lang['Last comments on albums'] = 'Friss hozzászólások az albumokban';
 $lang['Display comments on'] = 'Hozzászólások megjelenítése';
-?>
+$lang['%d comment on album'] = '%d hozzászólás az albumhoz';
+$lang['%d comments on albums'] = '%d hozzászólás az albumokhoz';
+$lang['Photos'] = 'Fényképek';
