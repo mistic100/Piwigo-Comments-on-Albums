245d350b2316c7fe394f0f4ce21605e13fa29163
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-28 18:54:50 +0100

    Update vi_VN, thanks to dummy

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d comment on album'] = '%d bình luận cho album';
+$lang['%d comments on albums'] = '%d bình luận cho album';
+$lang['Comments on albums'] = 'Bình luận cho album';
+$lang['Comments on photos'] = 'Bình luận cho ảnh';
+$lang['Display comments on'] = 'Hiện bình luận cho';
+$lang['Display last posted comments on albums'] = 'Hiện bình luận mới nhất cho album';
+$lang['Last comments on albums'] = 'Bình luận mới nhất cho album';
+$lang['Photos'] = 'Ảnh';
