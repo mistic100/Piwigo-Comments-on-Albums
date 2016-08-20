35dbb29fc97a75aa52c3391a06692975dd5ab8b5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-20 07:56:34 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -1,11 +1,10 @@
 <?php
 
 $lang['Comments on albums'] = 'Commentaar op albums';
-$lang['Comments on photos'] = 'Commentaar op afbeeldingen';
-$lang['Display last posted comments on albums'] = 'Geef de laatste commentaren op albums weer';
+$lang['Comments on photos'] = 'Commentaar op foto\'s';
+$lang['Display last posted comments on albums'] = 'Toon de laatste commentaren op albums';
 $lang['Last comments on albums'] = 'De laatste commentaren op albums';
-$lang['Display comments on'] = 'Geef reacties weer op';
+$lang['Display comments on'] = 'Toon reacties op';
 $lang['%d comment on album'] = '%d commentaar op album';
 $lang['%d comments on albums'] = '%d commentaar op albums';
-$lang['Photos'] = 'Fotos';
-?>
+$lang['Photos'] = 'Foto\'s';
