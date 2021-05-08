669939c6402d756a46a532db0b26ff9490635c4d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-05-08 23:14:34 +0200

    Add ja_JP, thanks to Toramaru13

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Display last posted comments on albums'] = 'アルバムへの最新のコメントを表示する';
+$lang['Last comments on albums'] = 'アルバムへの最新のコメント';
+$lang['Photos'] = '写真';
+$lang['%d comment on album'] = 'アルバムへのコメント%d件';
+$lang['%d comments on albums'] = 'アルバムへのコメント%d件';
+$lang['Comments on albums'] = 'アルバムへのコメント';
+$lang['Comments on photos'] = '写真へのコメント';
+$lang['Display comments on'] = 'コメントを表示する箇所';
